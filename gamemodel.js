b853397c815273ff7926777a65ced6041cc607af
Michael Tong <michi.tong@gmail.com>
2024-02-13 03:28:57 +0100

    fixed gamemodel

--- gamemodel.js
+++ gamemodel.js
@@ -1,55 +1,55 @@
 class GameModel {
   constructor(ctx) {
-    this.ctx = ctx
-    this.fallingPiece = null // piece
-    this.grid = this.makeStartingGrid()
-}
-
-makeStartingGrid() {
-  let grid = []
-  for (var i = 0; i < ROWS; i++) {
-    grid.push([])
-    for  (var j = 0; j < COLS; j++) {
-      grid[grid.length -1].push(0)
-    }
+      this.ctx = ctx
+      this.fallingPiece = null // piece
+      this.grid = this.makeStartingGrid()
   }
-  return grid
-}
 
-collision(x, y) {
-  const shape = this.fallingPiece.shape
-  const n = shape.length
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (shape[i][j] > 0) {
-        let p = x + j
-        let q = y + i
-        if (p >= 0 && p < COLS && q < ROWS) {
-          // check if the current piece is in bounds
-          if (this.grid[q][p] > 0) {
-            return true
+  makeStartingGrid() {
+      let grid = []
+      for (var i = 0; i < ROWS; i++) {
+          grid.push([])
+          for (var j = 0; j < COLS; j++) {
+              grid[grid.length - 1].push(0)
           }
-          } else {
-            return true
-        }
       }
-    }
+      return grid
   }
-    return false
+
+  collision(x, y, candidate=null) {
+      const shape = candidate || this.fallingPiece.shape
+      const n = shape.length
+      for (let i = 0; i < n; i++) {
+          for (let j = 0; j < n; j++) {
+              if (shape[i][j] > 0) {
+                  let p = x + j
+                  let q = y + i
+                  if (p >= 0 && p < COLS && q < ROWS) {
+                      // in bounds
+                      if (this.grid[q][p] > 0) {
+                          return true
+                      }
+                  } else {
+                      return true
+                  }
+              }
+          }
+      }
+      return false
   }
 
   renderGameState() {
-    for (let i = 0; i < this.grid.length; i++) {
-      for (let j = 0; j < this.grid[i].length; j++) {
-        let cell = this.grid[i][j]
-        this.ctx.fillStyle = COLORS[cell]
-        this.ctx.fillRect(j, i, 1, 1)
+      for (let i = 0; i < this.grid.length; i++) {
+          for (let j = 0; j < this.grid[i].length; j++) {
+              let cell = this.grid[i][j]
+              this.ctx.fillStyle = COLORS[cell]
+              this.ctx.fillRect(j, i, 1, 1)
+          }
       }
-    }
 
-    if (this.fallingPiece !== null) {
-      this.fallingPiece.renderPiece()
-    }
+      if (this.fallingPiece !== null) {
+          this.fallingPiece.renderPiece()
+      }
   }
 
 
@@ -62,62 +62,64 @@ collision(x, y) {
           const x = this.fallingPiece.x
           const y = this.fallingPiece.y
           shape.map((row, i) => {
-            row.map((cell, j) => {
-                let p = x + j
-                let q = y + i
-                if (p >= 0 && p < COLS && q < ROWS && cell > 0) {
-                    this.grid[q][p] = shape[i][j]
-                }
-            })
+              row.map((cell, j) => {
+                  let p = x + j
+                  let q = y + i
+                  if (p >= 0 && p < COLS && q < ROWS && cell > 0) {
+                      this.grid[q][p] = shape[i][j]
+                  }
+              })
           })
 
-        //GameOver check
-        if (this.fallingPiece.y === 0) {
-          alert("Game Over!")
-          this.grid = this.makeStartingGrid()
-        }
-        this.fallingPiece = null
+          // check game over
+          if (this.fallingPiece.y === 0) {
+              alert("Game over!")
+              this.grid = this.makeStartingGrid()
+          }
+          this.fallingPiece = null
       } else {
-        this.fallingPiece.y += 1
+          this.fallingPiece.y += 1
       }
       this.renderGameState()
+  }
+
+  move(right) {
+      if (this.fallingPiece === null) {
+          return
       }
 
-      move(right) {
-        if (this.fallingPiece === null) {
-            return
+      let x = this.fallingPiece.x
+      let y = this.fallingPiece.y
+      if (right) {
+          // move right
+          if (!this.collision(x + 1, y)) {
+              this.fallingPiece.x += 1
           }
+      } else {
+          // move left
+          if (!this.collision(x - 1, y)) {
+              this.fallingPiece.x -= 1
+          }
+      }
+      this.renderGameState()
+  }
 
-          let x = this.fallingPiece.x
-          let y = this.fallingPiece.y
-          if (right) {
-            //move right
-            if (!this.collision(x + 1, y)) {
-                this.fallingPiece.x += 1
-          }
-          } else {
-              //move left
-              if (!this.collision(x - 1 , y)) {
-                this.fallingPiece.x -= 1
+  rotate() {
+      if (this.fallingPiece !== null) {
+          let shape = [...this.fallingPiece.shape.map((row) => [...row])]
+          // transpose of matrix
+          for (let y = 0; y < shape.length; ++y) {
+              for (let x = 0; x < y; ++x) {
+                  [shape[x][y], shape[y][x]] =
+                  [shape[y][x], shape[x][y]]
               }
           }
-          this.renderGameState()
-    }
-
-    rotate() {
-      if (this.fallingPiece !== null) {
-        let shape = this.fallingPiece.shape
-        //transpose of matrix
-        for (let y = 0; y < Shape.length; ++y){
-          for (let x = 0; x < y; ++x) {
-              [this.fallingPiece.shape[x][y], this.fallingPiece.shape[y][x]]=
-              [this.fallingPiece.shape[y][x], this.fallingPiece.shape[x][y]]
+          // reverse order of rows
+          shape.forEach((row => row.reverse()))
+          if (!this.collision(this.fallingPiece.x, this.fallingPiece.y, shape)) {
+              this.fallingPiece.shape = shape
           }
-        }
-
-        //reverse the order of rows
-        this.fallingPiece.shape.forEach((row => row.reverse()))
       }
       this.renderGameState()
-    }
   }
+}
