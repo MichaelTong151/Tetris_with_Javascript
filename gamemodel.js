170f35fbb3d56078defd4b63ce41f8a1a4677e47
Michael Tong <michi.tong@gmail.com>
2024-02-13 01:37:34 +0100

    added collisionDetection, renderGameState, moveDown, GameOverCheck

--- gamemodel.js
+++ gamemodel.js
@@ -16,5 +16,70 @@ makeStartingGrid() {
   return grid
 }
 
+collision(x, y) {
+  const shape = this.fallingPiece.shape
+  const n = shape.length
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (shape[i][j] > 0) {
+        let p = x + j
+        let q = y + i
+        if (p >= 0 && p < COLS && q < ROWS) {
+          // check if the current piece is in bounds
+          if (this.grid[q][p] > 0) {
+            return true
+          }
+          } else {
+            return true
+        }
+      }
+    }
+  }
+    return false
+  }
 
-}
+  renderGameState() {
+    for (let i = 0; i < this.grid.length; i++) {
+      for (let j = 0; jk < this.grid[i].length; j++) {
+        let cell = this.grid[i][j]
+        this.ctx.fillStyle = COLORS[cell]
+        this.ctx.fillRect(j, i, 1, 1)
+      }
+    }
+
+    if (this.fallingPiece !== null) {
+      this.fallingPiece.renderPiece()
+    }
+  }
+
+
+  moveDown() {
+      if (this.fallingPiece === null) {
+          this.renderGameState()
+          return
+      } else if (this.collision(this.fallingPiece.x, this.fallingPiece.y + 1)) {
+          const shape = this.fallingPiece.shape
+          const x = this.fallingPiece.x
+          const y = thisfallingPiece.y
+          shape.map((row, i) => {
+            row.map((cell, j) => {
+                let p = x + j
+                let q = y + i
+                if (p >= 0 && p < COLS && q < ROWS && cell > 0) {
+                    this.grid[q][p] = shape[i][j]
+                }
+            })
+          })
+
+        //GameOver check
+        if (this.fallingPiece.y === 0) {
+          alert("Game Over!")
+          this.grid = this.makeStartingGrid()
+        }
+        this.fallingPiece = null
+      } else {
+        this.fallingPiece.y += 1
+      }
+      this.renderGameState()
+      }
+    }
